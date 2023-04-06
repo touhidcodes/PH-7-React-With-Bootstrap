30e078279b8b1624ddbeef2c62cb2462f11a3205
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-06 22:08:53 +0600

    use-react-bs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -28,54 +29,7 @@ function App() {
 			<p className='read-the-docs'>
 				Click on the Vite and React logos to learn more
 			</p>
-			<button type='button' className='btn btn-primary'>
-				Primary
-			</button>
-			<button type='button' className='btn btn-secondary'>
-				Secondary
-			</button>
-			<button type='button' className='btn btn-success'>
-				Success
-			</button>
-			<button type='button' className='btn btn-danger'>
-				Danger
-			</button>
-			<button type='button' className='btn btn-warning'>
-				Warning
-			</button>
-			<button type='button' className='btn btn-info'>
-				Info
-			</button>
-			<button type='button' className='btn btn-light'>
-				Light
-			</button>
-			<button type='button' className='btn btn-dark'>
-				Dark
-			</button>
-			<button type='button' className='btn btn-primary'>
-				Primary
-			</button>
-			<button type='button' className='btn btn-secondary'>
-				Secondary
-			</button>
-			<button type='button' className='btn btn-success'>
-				Success
-			</button>
-			<button type='button' className='btn btn-danger'>
-				Danger
-			</button>
-			<button type='button' className='btn btn-warning'>
-				Warning
-			</button>
-			<button type='button' className='btn btn-info'>
-				Info
-			</button>
-			<button type='button' className='btn btn-light'>
-				Light
-			</button>
-			<button type='button' className='btn btn-dark'>
-				Dark
-			</button>
+			<Button variant='danger'>My Button</Button>
 			<div className='card'>
 				<img src='...' className='card-img-top' alt='...' />
 				<div className='card-body'>
